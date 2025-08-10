f9bd7fda2974b4584d8085e62d1b678e70f9db2f
gu <13598957715@126.com>
2025-08-10 16:54:30 +0800

    rust类型测试

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+    test();
+}
+
+fn test() {
+    let nice_count: i32 = 100;
+    let nice_number: i64 = 54;
+
+    println!("nice_count :{}", nice_count);
+    println!("nice_number :{}", nice_number);
+
+    // nice_count = 20;   不能修改
+
+    let mut count = 3;
+    count = count + 1;
+    println!("count :{}", count);
+
+    let mut x = "hello";
+    println!("x : {}", x);
+
+    x = "world";
+    println!("x : {}", x);
 }
