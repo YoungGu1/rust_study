d4fbfdcb06216a8a4c6ccdaa10a3abd3034b5c25
gu <13598957715@126.com>
2025-08-11 20:09:58 +0800

    包

--- my_package/src/main.rs
+++ my_package/src/main.rs
@@ -1,7 +1,17 @@
 
-fn main() {
-    
+//声明一个模块people  会去找people.rs文件
+//如果没有people.rs文件会去找people/mod.rs文件
+//如果没有people/mod.rs文件会去找people目录下的lib.rs文件
+mod people;
 
+// 引入people模块中的People结构体
+use people::People;
+
+fn main() {
+    let person = People::new("张三".to_string(), 25);
+    person.introduce();
     
+    let another_person = People::new("李四".to_string(), 30);
+    another_person.introduce();
 
 }
--- my_package/src/people.rs
+++ my_package/src/people.rs
@@ -1,9 +1,12 @@
+//结构体 有两个公开的字段 姓名和年龄
 pub struct People {
     pub name: String,
     pub age: u32,
 }
 
+//
 impl People {
+    // 创建一个新的People实例 构造方法
     pub fn new(name: String, age: u32) -> Self {
         People { name, age }
     }
